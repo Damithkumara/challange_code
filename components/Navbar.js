f09cedaa0de10471db8bc4753c88010d05ed1978
Damithkumara <damithkumararoxx55@gmail.com>
2022-05-12 10:22:17 +0530

    1st commits

---
+++ components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {transition} from '@headlessui/react'; //smooth transition
+import {Link} from "react-scroll"; // smooth scroll through the page
+
+function Navbar() {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
